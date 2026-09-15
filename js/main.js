5cd58d632a15c101f05e15107ea8350a0e7560b9
THE GAME TIMES <habibifamily786@gmail.com>
2026-09-15 11:48:35 +0000

    Add fetch call to load content from JSON file

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+fetch("data/content.json")
+  .then(response => response.json())
+  .then(data => {
+    console.log("TGT content loaded:", data);
+  })
+  .catch(error => {
+    console.error("TGT content failed to load:", error);
+  });
